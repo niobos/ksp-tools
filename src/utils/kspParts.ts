e787e86404218505f6201f054b89b7577f0d0015
Niobos <niobos@dest-unreach.be>
2022-01-31 21:43:03 +0100

    Add more parts

--- src/utils/kspParts.ts
+++ src/utils/kspParts.ts
@@ -100,7 +100,7 @@ export default class Part extends Data {
             content,
         });
     }
-};
+}
 
 export class FuelTank extends Part {}
 export const fuelTanks = {
@@ -225,4 +225,65 @@ export const engines = {
     'Separatron I': Engine.create({cost: 75, mass: 0.0725, size: new Set([Size.RADIAL]), gimbal: 0, thrust: [13.792, 18], isp: [118, 154], consumption: Resources.create({sf: 1.589}), content: Resources.create({sf: 8}), throttleControl: false, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Sepratron_I"}),
     'T-1 Aerospike "Dart"': Engine.create({cost: 3850, mass: 1.0, size: new Set([Size.SMALL, Size.RADIAL]), gimbal: 0, thrust: [153.529, 180], isp: [290, 340], consumption: Resources.create({lf: 4.859, ox: 5.938, el: -5}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/T-1_Toroidal_Aerospike_%22Dart%22_Liquid_Fuel_Engine"}),
     'THK "Pollux"': Engine.create({cost: 6000, mass: 51.5, size: new Set([Size.MEDIUM, Size.RADIAL]), gimbal: 0, thrust: [1155.556, 1300], isp: [200, 225], consumption: Resources.create({sf: 78.556}), content: Resources.create({sf: 5800}), throttleControl: false, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/THK_%22Pollux%22_Solid_Fuel_Booster"}),
+};
+
+export class SolarPanel extends Part {
+    /* Power generator dependent on the distance to Kerbol
+     * and night/eclipses
+     */
+}
+export class FuelCell extends Part {
+    /* Throttlable power generator
+     */
+}
+export const electricalGenerators = {
+    'Gigantor XL Solar Array': SolarPanel.create({cost: 3000, mass: 0.3, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -24.4}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Gigantor_XL_Solar_Array"}),
+    'OX-4W 3x2 Photovoltaic Panels': SolarPanel.create({cost: 380, mass: 0.0175, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -1.64}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/OX-4W_3x2_Photovoltaic_Panels"}),
+    'OX-4L 1x6 Photovoltaic Panels': SolarPanel.create({cost: 380, mass: 0.0175, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -1.64}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/OX-4L_1x6_Photovoltaic_Panels"}),
+    'SP-W 3x2 Photovoltaic Panels': SolarPanel.create({cost: 440, mass: 0.025, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -1.64}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/SP-W_3x2_Photovoltaic_Panels"}),
+    'SP-L 1x6 Photovoltaic Panels': SolarPanel.create({cost: 440, mass: 0.025, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -1.64}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/SP-L_1x6_Photovoltaic_Panels"}),
+    'OX-STAT Photovoltaic Panels': SolarPanel.create({cost: 75, mass: 0.005, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -0.35}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/OX-STAT_Photovoltaic_Panels"}),
+    'OX-STAT-XL Photovoltaic Panels': SolarPanel.create({cost: 600, mass: 0.04, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -2.80}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/OX-STAT-XL_Photovoltaic_Panels"}),
+    'PB-NUK Radioisotope Thermoelectric Generator': Part.create({cost: 23300, mass: 0.08, size: new Set([Size.TINY]), consumption: Resources.create({el: -0.75}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/PB-NUK_Radioisotope_Thermoelectric_Generator"}),
+    'Fuel Cell': FuelCell.create({cost: 750, mass: 0.05, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -1.50, lf: 0.0016875, ox: 0.0020625}), content: Resources.create({el: 50}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Fuel_Cell"}),
+    'Fuel Cell Array': FuelCell.create({cost: 4500, mass: 0.24, size: new Set([Size.RADIAL]), consumption: Resources.create({el: -18.0, lf: 0.02025, ox: 0.02475}), content: Resources.create({el: 300}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Fuel_Cell_Array"}),
+};
+export const batteries = {
+    // Same energy/mass ratio
+    // Z-100 has lowest cost/energy ratio; Z-200 has highest cost/energy ratio
+    'Z-100': Part.create({cost: 80, mass: 0.005, size: new Set([Size.RADIAL]), content: Resources.create({el: 100}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Z-100_Rechargeable_Battery_Pack"}),
+    'Z-200': Part.create({cost: 360, mass: 0.01, size: new Set([Size.TINY]), content: Resources.create({el: 200}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Z-200_Rechargeable_Battery_Bank"}),
+    'Z-400': Part.create({cost: 550, mass: 0.02, size: new Set([Size.RADIAL]), content: Resources.create({el: 400}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Z-400_Rechargeable_Battery"}),
+    'Z-1k': Part.create({cost: 880, mass: 0.05, size: new Set([Size.SMALL]), content: Resources.create({el: 1000}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Z-1k_Rechargeable_Battery_Bank"}),
+    'Z-4k': Part.create({cost: 4500, mass: 0.3, size: new Set([Size.LARGE]), content: Resources.create({el: 4000}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Z-4K_Rechargeable_Battery_Bank"}),
+};
+
+export class ReactionWheel extends Part {
+    maxTorque: [number, number, number] = [0, 0, 0];  // pitch, yaw, roll
+    torquePowerRequirement: number = undefined;  // ⚡/s / (kN*m)
+}
+
+export class Pod extends ReactionWheel {
+    hibernateMultiplier: number = undefined;  // no hibernate
+
+    get consumptionHibernated() {
+        if(this.hibernateMultiplier === undefined) return this.consumption;
+        // else:
+        return this.consumption.copy({el: this.consumption.el * this.hibernateMultiplier})
+    }
+}
+export const probeCores = {
+    // TODO: check hibernate
+    'Probodobodyne RoveMate': Pod.create({cost: 800, mass: 0.15, size: new Set([Size.SMALL]), consumption: Resources.create({el: 2.40/60}), content: Resources.create({el: 120}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_RoveMate"}),
+    'Probodobodyne Stayputnik': Pod.create({cost: 300, mass: 0.05, size: new Set([Size.TINY]), consumption: Resources.create({el: 1.67/60}), content: Resources.create({el: 10}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_Stayputnik"}),
+    'Probodobodyne QBE': Pod.create({cost: 360, mass: 0.07, size: new Set([Size.TINY]), consumption: Resources.create({el: 1.50/60}), content: Resources.create({el: 5}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_QBE"}),
+    'Probodobodyne HECS': Pod.create({cost: 650, mass: 0.10, size: new Set([Size.TINY]), consumption: Resources.create({el: 1.50/60}), content: Resources.create({el: 10}), maxTorque: [0.3, 0.3, 0.3], torquePowerRequirement: 0.1, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_HECS"}),
+    'Probodobodyne OKTO': Pod.create({cost: 450, mass: 0.10, size: new Set([Size.TINY]), consumption: Resources.create({el: 1.20/60}), content: Resources.create({el: 10}), maxTorque: [0.3, 0.3, 0.3], torquePowerRequirement: 0.1, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_OKTO"}),
+    'Probodobodyne OKTO2': Pod.create({cost: 1480, mass: 0.04, size: new Set([Size.TINY]), consumption: Resources.create({el: 1.80/60}), content: Resources.create({el: 5}), wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_OKTO2"}),
+    'Probodobodyne HECS2': Pod.create({cost: 7500, mass: 0.20, size: new Set([Size.SMALL]), consumption: Resources.create({el: 3.00/60}), content: Resources.create({el: 1000}), maxTorque: [10, 10, 10], torquePowerRequirement: 0.050, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/Probodobodyne_HECS2"}),
+    'RC-001S Remote Guidance Unit': Pod.create({cost: 2250, mass: 0.10, size: new Set([Size.SMALL]), consumption: Resources.create({el: 3.0/60}), content: Resources.create({el: 15}), maxTorque: [0.5, 0.5, 0.5], torquePowerRequirement: 0.060, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/RC-001S_Remote_Guidance_Unit"}),
+    'RC-L01 Remote Guidance Unit': Pod.create({cost: 3400, mass: 0.50, size: new Set([Size.LARGE]), consumption: Resources.create({el: 4.80/60}), content: Resources.create({el: 30}), maxTorque: [1.5, 1.5, 1.5], torquePowerRequirement: 0.100, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/RC-L01_Remote_Guidance_Unit"}),
+    'MK2 Drone Core': Pod.create({cost: 2700, mass: 0.20, size: new Set([Size.MK2]), consumption: Resources.create({el: 3.0/60}), content: Resources.create({el: 250}), maxTorque: [15, 3, 3], torquePowerRequirement: 0.033, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/MK2_Drone_Core"}),
+    'MPO Probe': Pod.create({cost: 9900, mass: 0.90, size: new Set([Size.SMALL]), consumption: Resources.create({el: 3.00/60}), content: Resources.create({lf: 45, ox: 55, el: 1000}), maxTorque: [6, 6, 6], torquePowerRequirement: 0.050, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/MPO_Probe"}),
+    'MTM Stage': Pod.create({cost: 21500, mass: 0.80, size: new Set([Size.SMALL, Size.TINY]), consumption: Resources.create({el: 1.80/60}), content: Resources.create({xe: 3800, el: 4000}), maxTorque: [12, 12, 12], torquePowerRequirement: 0.050, wikiUrl: "https://wiki.kerbalspaceprogram.com/wiki/MTM_Stage"}),
 };
