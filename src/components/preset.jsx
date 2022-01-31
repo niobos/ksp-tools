7daa6ec41dc35e047e147ab0fe013e0615bad8b3
Niobos <niobos@dest-unreach.be>
2022-01-31 21:41:59 +0100

    Minor tweaks

--- src/components/preset.jsx
+++ src/components/preset.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function normalizeProps(props) {
     const defaultProps = {
-        // value:   // mutually exclusive with option
-        // option:  // mutually exclusive with value
+        // value:   // selected value, mutually exclusive with option
+        // option:  // selected name, mutually exclusive with value
         onChange: (value, optionName) => null,
         options: {},  // {"visible name": value, "group name": {"nested name": 42}}
         customName: "custom",  // name for the custom option
@@ -22,15 +22,13 @@ export default function Preset(props) {
         if(typeof props.options[option] === "object") {
             const suboptions = [];
             for(const suboption in props.options[option]) {
-                const value = '' + options[option][suboption];
-                suboptions.push(<option key={suboption} value={value}>{suboption}</option>)
-                flattened_options[suboption] = value;
+                suboptions.push(<option key={suboption} value={options[option][suboption]}>{suboption}</option>)
+                flattened_options[suboption] = options[option][suboption];
             }
             option_els.push(<optgroup key={option} label={option}>{suboptions}</optgroup>)
         } else {
-            const value = '' + props.options[option];
-            option_els.push(<option key={option} value={value}>{option}</option>)
-            flattened_options[option] = value;
+            flattened_options[option] = props.options[option];
+            option_els.push(<option key={option} value={props.options[option]}>{option}</option>)
         }
     }
 
