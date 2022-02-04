ff33e9d1624da39672e744da1aa12c6095c3309a
Niobos <niobos@dest-unreach.be>
2022-02-04 18:19:12 +0100

    Convert kspIcon to TypeScript

--- src/components/kspIcon.tsx
+++ src/components/kspIcon.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+
+// @ts-ignore
 import fundSvg from "../img/fund.svg";
 
-export function KspFund(props) {
+export function KspFund() {
     return <img src={fundSvg} alt="Kerbal Fund" style={{width: '1em', verticalAlign: 'middle'}}/>;
 }
