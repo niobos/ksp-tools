e21c793863bf16e922e31e47b349c36690b5c55b
Niobos <niobos@dest-unreach.be>
2025-01-17 10:38:44 +0100

    useThrottled: Better typing

--- src/utils/useThrottled.ts
+++ src/utils/useThrottled.ts
@@ -1,8 +1,11 @@
 import {useCallback, useRef} from "react";
 
-export default function useThrottled(callback, limit: number) {
+export default function useThrottled<TParams extends any[]>(
+    callback: (...args: TParams) => void,
+    limit: number
+): (...args: TParams) => void {
     const lastCallRef = useRef(0)
-    const throttledCallback = useCallback((...args) => {
+    const throttledCallback = useCallback((...args: TParams) => {
         const now = Date.now()
         if (now - lastCallRef.current >= limit) {
             lastCallRef.current = now
